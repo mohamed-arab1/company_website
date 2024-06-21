b7913f141d2ff7c211c72b9fc073f220c93c42f4
islamabozeed <islammahmoud13@gmail.com>
2024-06-21 19:52:24 +0300

    navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import  BookService  from "./pages/BookService";
 import Registration from "./pages/Registration";
+import Layout from "./components/Layout";
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
+           <Route element={<Layout />}>
           <Route index path='/' element={<LandingPage />} />
+          <Route path='/book-service' element={<BookService />}></Route>
+          </Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/registration' element={<Registration />}></Route>
-          <Route path='/book-service' element={<BookService />}></Route>
         </Route>
     )
   )
 
   return (
-    <>
+    <div>
     <RouterProvider router={router} />
-    </>
+    </div>
   )
 }
 
