a5609097b2bcfe230569a42bed3d6711c4576025
mohamed-arab1 <momenmoslem991@gmail.com>
2024-06-24 19:54:30 +0300

    updated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,15 +2,20 @@ import Contact from './../components/Contact';
 import Home from '../components/Home';
 import Testimonials from '../components/Testimonials';
 import Why_us from '../components/Why_us';
-
+import Features from "../components/Features"
+import Numbers from "../components/Numbers"
+import Services from "../components/Services"
 
 const LandingPage = () => {
   return (
     <main>
            <Home/>
+           <Why_us/>
+           <Features />
+           <Services />
+           <Numbers />
            <Testimonials/>
            <Contact />
-           <Why_us/>
     </main>
   )
 }
