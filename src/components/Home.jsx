238ca203ae73e81b49d15ce8519a10d4be9232b5
laila <lailaamr2>
2024-07-04 02:05:23 +0300

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,23 @@ export default function Home(){
     return(
         <>
         <section id="home" className=" w-full h-screen bg-[#002f69] ">
-        <dev   className=" home_grid  grid grid-cols-2 gap-2 mx-auto">
-            <dev className=" mt-20 ml-10"> 
-                <img src={home_photo} className="home_photo mx-auto" height={590}  width={620} alt="home photo" />
-            </dev>
-            <dev className=" mt-24 ml-10 animate__animated animate__fadeIn mx-auto">
-            <dev className="flex items-center gap-3 ">
+        <div   className=" home_grid  grid grid-cols-2 gap-2 mx-auto">
+            <div className=" mt-20 ml-10"> 
+                <img src={home_photo} className="home_photo mx-auto" height={570}  width={620} alt="home photo" />
+            </div>
+            <div className=" mt-24 ml-10 animate__animated animate__fadeIn mx-auto">
+            <div className="flex items-center gap-3 ">
             <img src={logo} alt="logo"  />
             <h3 className="text-white" >Vandemar Construction</h3>
-            </dev>
+            </div>
 
             <h2 className="home_title text-[84px] text-white font-bold">
                 Let's make your vision come to life
             </h2>
             
             <button className="bg-[#387fd1] p-4 cursor-pointer btn font-bold text-[#ffffff]">Learn More</button>          
-            </dev>
-        </dev>
+            </div>
+        </div>
         </section>
         </>
 
